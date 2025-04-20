b76447ade955ed626d200f7d8c6f4b798dc6c74c
hgfhhffhyvgfcjuhgvc <slvbo12@gmail.com>
2025-04-20 18:07:26 +0400

    Update sw-v2.js

--- sw-v2.js
+++ sw-v2.js
@@ -1,2 +1,19 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match('index.html'))));
+const CACHE = 'dpt-cache-v1';
+const CORE = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/tap-soft.mp3',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
